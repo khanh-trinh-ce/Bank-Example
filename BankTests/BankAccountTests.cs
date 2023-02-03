e8108a84dee0300a0825ba6e7e022a38885534a7
FIXED-TERM Nguyen Viet Khanh Trinh (MS/EBS12) <gur3hc@bosch.com>
2023-02-03 07:55:42 +0700

    Signed-off-by: FIXED-TERM Nguyen Viet Khanh Trinh (MS/EBS12) <gur3hc@bosch.com>

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,30 @@
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Gomez Addams", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly!");
+        }
+    }
+}
